1ce42260cbbfb20c7b5360005c020eb8d2a9c7d7
Jacob Goh Chuan Ching <jacobgoh101@gmail.com>
2015-12-06 15:14:20 +0800

    First Commit

---
+++ Gruntfile.js
@@ -0,0 +1,51 @@
+// npm install
+// npm install grunt-postcss pixrem autoprefixer cssnano
+
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    sass: {
+      dist: {
+        options:{
+          style:'compressed'
+        },
+        files: {
+          'style.css' : 'sass/style.scss'
+        }
+      }
+    },
+    postcss: {
+      options: {
+      map: true, // inline sourcemaps      
+
+      processors: [
+        require('pixrem')(), // add fallbacks for rem units
+        require('autoprefixer')({browsers: 'last 2 versions'}), // add vendor prefixes
+        require('cssnano')() // minify the result
+        ]
+      },
+      files: {
+        'style.css' : 'style.css'
+      }
+    },
+    notify: {
+      postcss: {
+        options: {
+          title: 'Task Complete',  // optional
+          message: 'Sass and PostCSS finished running', //required
+        }
+      }
+    },
+    watch: {
+      set1: {
+        files: 'sass/**/*.scss',
+        tasks: ['sass', 'postcss', 'notify']
+      }
+    }
+  });
+grunt.loadNpmTasks('grunt-contrib-sass');
+grunt.loadNpmTasks('grunt-contrib-watch');
+grunt.loadNpmTasks('grunt-postcss');
+grunt.loadNpmTasks('grunt-notify');
+grunt.registerTask('default',['watch']);
+}
---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Basic_Underscores
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'basic-underscores' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'basic-underscores' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'basic-underscores' ), 'basic-underscores', '<a href="http://jacobgoh.com" rel="designer">Jacob Goh</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>

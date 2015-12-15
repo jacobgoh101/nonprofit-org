c8f3cff6465f86ab8015509d7cd6fa0ebbb30c76
Jacob Goh Chuan Ching <jacobgoh101@gmail.com>
2015-12-15 16:41:23 +0800

    Front Page Flex Slider done

--- template-front-page.php
+++ template-front-page.php
@@ -1,6 +1,15 @@
 <?php /* Template Name: Front Page */ 
 
+//Add FlexSlider's styles and script
+function enqueue_flexslider_styles_scripts() { 
+	wp_enqueue_style('flexslider-css', get_template_directory_uri() . '/woothemes-FlexSlider-9a419a0/flexslider.css');
+	wp_enqueue_script( 'flexslider-js', get_template_directory_uri() . '/woothemes-FlexSlider-9a419a0/jquery.flexslider-min.js', array('jquery'), null, true );
+} 
+
+add_action('wp_enqueue_scripts', 'enqueue_flexslider_styles_scripts',1);
+
 get_header(); 
+
 
 ?>
 
@@ -39,6 +48,80 @@ get_header();
 	<a href="#">Where are we going</a>
 </div>
 
+<!-- Flex Slider -->
+<?php
+// The Query - story post type
+$the_query1 = new WP_Query( array( 'post_type' => 'story',
+	'posts_per_page' => 8
+	));
+
+// The Loop
+if ( $the_query1->have_posts() ) {
+	echo '<div class="flex-slider-wrap">';
+	echo '<div id="slider" class="flexslider"><ul class="slides">';
+	while ( $the_query1->have_posts() ) {
+		$the_query1->the_post();
+		?>
+		<li>
+			<div class="side-image">
+				<div href="<?php the_permalink() ?>" class="images-wrapper" style="background-image: url('<?php the_field('Portrait'); ?>');"></div>
+				<div class="side-image-content">
+					<h1><?php the_title(); ?></h1>
+					<p><?php echo substr(get_the_excerpt(), 0,300) . "..."; ?></p>
+					<a href="<?php the_permalink() ?>">read more about <?php the_title() ?></a>
+				</div>
+			</div>
+		</li>
+		<?php
+	}
+	echo '</ul></div>';
+
+	wp_reset_postdata();
+
+	echo '<div id="carousel" class="flexslider"><ul class="slides">';
+	while ( $the_query1->have_posts() ) {
+		$the_query1->the_post();
+		?>
+		<li>
+			<img src="<?php the_field('Portrait'); ?>" />
+		</li>
+		<?php
+	}
+	echo '<li>	<img src="http://localhost/nonprofit-org/wp-content/themes/nonprofit-org/images/transparent-1px-1px.png" width:"200px" /></li><li>	<img src="http://localhost/nonprofit-org/wp-content/themes/nonprofit-org/images/transparent-1px-1px.png" width:"200px" /></li>';
+	echo '</ul></div>';
+}
+/* Restore original Post Data */
+wp_reset_postdata();
+echo '</div>'
+?>
+
+
+<script>
+	(function($) {
+		$(window).load(function() {
+  			// The slider being synced must be initialized first
+  			$('#carousel').flexslider({
+  				animation: "slide",
+  				controlNav: false,
+  				animationLoop: true,
+  				slideshow: false,
+  				itemWidth: 150,
+  				itemMargin: 5,
+  				asNavFor: '#slider'
+  			});
+
+  			$('#slider').flexslider({
+  				animation: "slide",
+  				controlNav: false,
+  				animationLoop: false,
+  				slideshow: false,
+  				sync: "#carousel"
+  			});
+  		});
+
+	})( jQuery );
+
+</script>
 
 <?php
 
